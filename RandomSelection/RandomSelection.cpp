ffde624c2d9fd3643382976ef4562cf372547430
MechaDragonX <sonicraghav@gmail.com>
2019-10-05 19:37:41 -0700

    Mess around with rng

---
+++ RandomSelection/RandomSelection.cpp
@@ -0,0 +1,52 @@
+﻿#include "RandomSelection.hpp"
+
+using namespace std;
+
+enum class Theme { HighScore, Upgrade, Teamwork, Discover, Character, Resources, Memory, Expansion, Risk };
+
+int select();
+void out(int);
+
+int main()
+{
+	out(select());
+	return 0;
+}
+int select()
+{
+	srand(time(NULL));
+	int random = rand() % 8;
+	return random;
+}
+void out(int random)
+{
+	cout << "The theme is ";
+	switch (static_cast<Theme>(random))
+	{
+	case Theme::HighScore:
+		cout << "\"High Score\"";
+		break;
+	case Theme::Upgrade:
+		cout << "\"Upgrade\"";
+		break;
+	case Theme::Teamwork:
+		cout << "\"Teamwork\"";
+		break;
+	case Theme::Discover:
+		cout << "\"Discover\"";
+		break;
+	case Theme::Resources:
+		cout << "\"Resources\"";
+		break;
+	case Theme::Memory:
+		cout << "\"Memory\"";
+		break;
+	case Theme::Expansion:
+		cout << "\"Expansion\"";
+		break;
+	case Theme::Risk:
+		cout << "\"Risk\"";
+		break;
+	}
+	cout << "!\nGood luck!\n";
+}
---
+++ RandomSelection/RandomSelection.hpp
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include <iostream>
+#include <time.h>
+
+// TODO: Reference additional headers your program requires here.
